d91685428b3bdf3a60e5704ce0544fb50250ad7b
yuta82644 <calendula6724yuta@gmail.com>
2024-06-30 18:01:26 +0900

    todo_new.htmlの作成

--- app/controllers/route_main.go
+++ app/controllers/route_main.go
@@ -5,7 +5,7 @@ import (
 	"log"
 	"net/http"
 	// "log"
-	// "github.com/yuta82644/go-todo_app/app/app/models"
+	// "github.com/yuta82644/go-todo_app/app/models"
 )
 
 func top(w http.ResponseWriter, r *http.Request) {
@@ -31,3 +31,12 @@ func top(w http.ResponseWriter, r *http.Request) {
 		generateHTML(w, user, "layout", "private_navbar", "index")
 	}
   }
+
+  func todoNew(w http.ResponseWriter, r *http.Request) {
+	_, err := session(w, r)
+	if err != nil {
+		http.Redirect(w, r, "/login", http.StatusFound)
+	} else {
+		generateHTML(w, nil, "layout", "private_navbar", "/todo_new")
+	}
+}
--- app/controllers/server.go
+++ app/controllers/server.go
@@ -49,6 +49,7 @@ func StartMainServer() error {
     http.HandleFunc("/authenticate", authenticate)
 	http.HandleFunc("/logout", logout)
     http.HandleFunc("/todos", index)
+	http.HandleFunc("/todos/new", todoNew)
 
     return http.ListenAndServe(":"+config.Config.Port, nil)
 }
