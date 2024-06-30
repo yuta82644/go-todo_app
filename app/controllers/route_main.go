6bfe4ec4542f201178a3a4c2ab8a1246bfc4f3cd
yuta82644 <calendula6724yuta@gmail.com>
2024-06-30 18:28:47 +0900

    indexに一覧usernameが表示されない状態memo

--- app/controllers/route_main.go
+++ app/controllers/route_main.go
@@ -39,4 +39,27 @@ func top(w http.ResponseWriter, r *http.Request) {
 	} else {
 		generateHTML(w, nil, "layout", "private_navbar", "/todo_new")
 	}
-}
+  }
+
+  	func todoSave(w http.ResponseWriter, r *http.Request) {
+		sess, err := session(w, r)
+		if err != nil {
+			http.Redirect(w, r, "/login", http.StatusFound)
+		} else {
+			err = r.ParseForm()
+			if err != nil {
+				log.Println(err)
+			}
+			user, err := sess.GetUserBySession()
+			if err != nil {
+				log.Println(err)
+			}
+
+			content := r.PostFormValue("content")
+			if err := user.CreateTodo(content); err != nil {
+				log.Println(err)
+			}
+
+			http.Redirect(w, r, "/todos", http.StatusFound)
+		}
+	}
